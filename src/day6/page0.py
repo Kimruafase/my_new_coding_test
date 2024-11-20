b0745a787924562dbc922244898d9b636b5e0640
kimruafase <ppooppplus33@gmail.com>
2024-11-20 23:28:22 +0900

    241109 23:28 김민석 커밋

--- src/day6/page0.py
+++ src/day6/page0.py
@@ -16,4 +16,20 @@ def string_count():
     s = sys.stdin.readline()
     print(len(s) - 1)
 
-string_count()
+# string_count()
+
+def string_first_last_print():
+    a = int(sys.stdin.readline())
+
+    for _ in range(a):
+        string = sys.stdin.readline()
+        print(f'{string[0]}{string[-2:-1]}')
+
+# string_first_last_print()
+
+
+def string_to_ascii():
+    s = input()
+    print(ord(s))
+
+# string_to_ascii()
